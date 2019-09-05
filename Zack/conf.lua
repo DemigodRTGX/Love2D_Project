d667dc8e5fefe36f07f9f294ceefff9b31f6b53a
zack9527 <zackzhang163@163.com>
2019-09-05 09:02:41 +0800

    新增

---
+++ Zack/conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.title = "My Love APP"
+    t.console = true
+    t.window.width = 320                -- The window width (number)
+    t.window.height = 240               -- The window height (number)
+    end
---
+++ Zack/main.lua
@@ -0,0 +1,41 @@
+-- menu==escape
+-- y=i
+-- a=j
+-- x=u
+-- b=k
+-- select=space
+-- start=return
+local qteView = require("qte")
+Components = {}
+Components["qte"] = qteView
+
+function love.load()
+    for k, Component in pairs(Components) do
+        Component:load();
+    end
+end
+
+function love.draw()
+    for k, Component in pairs(Components) do
+        Component:draw();
+    end
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit(0);
+        return;
+     end
+     if key == "space" then
+        qteView.enabled = not qteView.enabled;
+     end
+    for k, Component in pairs(Components) do
+        Component:keypressed(key);
+    end
+end
+
+function love.update(dt)
+    for k, Component in pairs(Components) do
+        Component:update(dt);
+    end
+end
